316814d74e5470ae0bf002beeb8a29125ae1776d
antonio-cajueiro-campos <kimeesan5@gmail.com>
2021-09-27 22:05:00 -0300

    push2

--- ex8.c
+++ ex8.c
@@ -2,11 +2,13 @@
 #include <math.h>
 
 void main() {
-	float delta, a, b, c;
+	float delta, a, b, c, x1, x2;
 
 	delta = pow(b, 2)-4*a*c;
 
 	if (delta < 0) {
-		
+		x1 = 0;
+	} else {
+		x2 = 0;
 	}
 }
